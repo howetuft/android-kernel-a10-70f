bbf4e20e30ced9ef550277fded9c1fa9e9b4be19
howetuft <howetuft@gmail.com>
2023-06-03 19:52:05 +0200

    Port merge-kconfig.py to python3

--- alps/mediatek/build/tools/config/merge-kconfig.py
+++ alps/mediatek/build/tools/config/merge-kconfig.py
@@ -10,7 +10,7 @@ kconfig_file.remove(sys.argv[0])
 for f in kconfig_file:
     ff = open(f)
     for line in ff.readlines(): 
-        result = (filter(lambda x:x,[x.search(line) for x in pattern]) or [None])[0]
+        result = (list(filter(lambda x:x,[x.search(line) for x in pattern])) or [None])[0]
         if not result: continue
         name,value = None,None
         name = result.group(1)
@@ -19,6 +19,6 @@ for f in kconfig_file:
     ff.close()
 
 for item in sorted(config.keys()):
-    if not config[item]: print "# %s is not set"%item
-    else: print "%s=%s"%(item,config[item])
+    if not config[item]: print("# %s is not set"%item)
+    else: print("%s=%s"%(item,config[item]))
 
